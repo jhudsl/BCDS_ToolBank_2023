80511498ee310e10c83368d04afd3c31fdac56b2
avahoffman <avamariehoffman@gmail.com>
2023-12-05 12:36:57 -0500

    Notes on what's going on

--- render.R
+++ render.R
@@ -1,5 +1,12 @@
 # Render the Final Report
 
+# To create the key, there is some funky spacing issues that need to be resolved.
+# Take the key from the GSA and read it in:
+# key <- jsonlite::fromJSON(readLines("key.json"))
+# Then, write it back. This gets rid of spacing issues:
+# write_json(key, path = "key_corrected.json")
+# then copy the contents of key_corrected.json into the github secret!
+
 # --------- Authenticate ---------
 
 # Note that creds need to be passed in the pull_request.yml or render-all.yml workflows first
